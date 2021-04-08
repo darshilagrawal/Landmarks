8c09b7fce588c52801258fea7bf4f1f86b999225
darshilagrawal <darshilagrawal2000@gmail.com>
2021-04-08 23:05:31 +0530

    Apple Introduction to SwiftUI Basics

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,10 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            MapView()
+                .frame(height:300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom,-130)
+            VStack(alignment: .leading) {
+                    
+                        Text("Turtle Rock")
+                            .font(.title)
+                    HStack {
+                        Text("Joshua Tree national Park")
+                            
+                        Spacer()
+                        Text("California")
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Kya Mast Park Banaya hai rey")
+                    
+            }.padding()
+            Spacer()
+        }
+            
+        }
+            
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
