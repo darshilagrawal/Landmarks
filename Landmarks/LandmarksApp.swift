37175f77068aa1895c36f08d4c919a6ff6006fa0
darshilagrawal <darshilagrawal2000@gmail.com>
2021-04-08 15:51:10 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Darshil Agrawal on 08/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
